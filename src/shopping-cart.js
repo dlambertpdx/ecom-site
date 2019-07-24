b003d6edace243f87f101fdb6dbd5054fd716e51
sanvean74 <dlambertpdx@gmail.com>
2019-07-23 23:05:10 -0700

    tests finally passing

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -1,12 +1,12 @@
-import order from './data/order.js';
+import cart from './data/order.js';
 import bikes from './data/products.js';
 import { findProduct } from './register.js';
 import renderLineItem from './render-line-item.js';
 
 const tbody = document.querySelector('tbody');
 
-for(let i = 0; i < order.length; i++) {
-    const lineItem = order[i];
+for(let i = 0; i < cart.length; i++) {
+    const lineItem = cart[i];
     const bike = findProduct(bikes, lineItem.code);
     const dom = renderLineItem(lineItem, bike);
 
--- test/render-line-item.test.js
+++ test/render-line-item.test.js
@@ -1,5 +1,5 @@
 
-import bikes from '../src/data/products.js'
+import bikes from '../src/data/products.js';
 import { findProduct } from '../src/register.js';
 import renderLineItem from '../src/render-line-item.js';
 
@@ -14,13 +14,13 @@ test('renders line item', (assert) => {
         quantity: 1
     };
     const bike = findProduct(bikes, lineItem.code);
-    const expect = '<tr><td>Road Bike</td><td>1</td><td>$500</td><td>$500</td></tr>';
+    const expect = '<tr><td class="item">Blue Road Bike</td><td class="qty">1</td><td class="subtotal">$300.00</td><td class="line-total">$300.00</td></tr>';
 
     // act 
-    const dom = renderLineItem(bike, lineItem);
+    const dom = renderLineItem(lineItem, bike);
     const html = dom.outerHTML;
 
     // assert
-    assert.equal(html, expect)
+    assert.equal(html, expect);
 
 });
